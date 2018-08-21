47fbd333265c9b4dd621b3be48fa7a874b4fd666
Viam Mercer <viam.mercer@hotmail.com>
2018-08-21 08:38:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   return playlist;
 }
+
+function removePlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
+}
